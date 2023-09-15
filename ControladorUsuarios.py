8adaebd7e5a446e3d4e824097e9d99ef753d8dc5
William Velasquez <wvelasquez@udemedellin.edu.co>
2023-09-15 06:37:42 -0500

    Separada la configuracion secreta en una clase aparte

--- ControladorUsuarios.py
+++ ControladorUsuarios.py
@@ -1,21 +1,35 @@
 """
+    Pertenece a la capa de Acceso a Datos
+
     Controla las operaciones de almacenamiento de la clase Usuario
 """
 from Usuario import Usuario
 
 import sys
 import psycopg2
+import SecretConfig
+
+def ObtenerCursor( ) :
+    """
+    Crea la conexion a la base de datos y retorna un cursor para ejecutar instrucciones
+    """
+    DATABASE = SecretConfig.DATABASE
+    USER = SecretConfig.USER
+    PASSWORD = SecretConfig.PASSWORD
+    HOST = SecretConfig.HOST
+    PORT = SecretConfig.PORT
+    connection = psycopg2.connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT)
+    return connection.cursor()
+
+    
 
 def Insertar( usuario : Usuario ):
     """ Guarda un Usuario en la base de datos """
 
-    # Reemplace los datos de conexion con los datos tomados de su servidor
-    connection = psycopg2.connect(database="neondb", user="ProfeBill", password="O4oU5naNIuRe", host="ep-round-mountain-97326230.us-east-2.aws.neon.tech", port=5432)
-        
     try:
 
         # Todas las instrucciones se ejecutan a tavés de un cursor
-        cursor = connection.cursor()
+        cursor = ObtenerCursor()
         cursor.execute(f"""
         insert into usuarios (
             cedula,   nombre,  apellido,  telefono,  correo, direccion, codigo_municipio, codigo_departamento
@@ -29,16 +43,16 @@ def Insertar( usuario : Usuario ):
         # Las instrucciones DDL y DML no retornan resultados, por eso no necesitan fetchall()
         # pero si necesitan commit() para hacer los cambios persistentes
 
-        connection.commit()
+        cursor.connection.commit()
     except  :
-        connection.rollback() 
+        cursor.connection.rollback() 
         raise Exception("No fue posible insertar el usuario : " + usuario.cedula )
     
 def BuscarPorCedula( cedula :str ):    
     """ Busca un usuario por el numero de Cedula """
-    connection = psycopg2.connect(database="neondb", user="ProfeBill", password="O4oU5naNIuRe", host="ep-round-mountain-97326230.us-east-2.aws.neon.tech", port=5432)
+
     # Todas las instrucciones se ejecutan a tavés de un cursor
-    cursor = connection.cursor()
+    cursor = ObtenerCursor()
     cursor.execute(f"SELECT cedula,nombre,apellido,correo,direccion,telefono,codigo_departamento,codigo_municipio from usuarios where cedula = '{cedula}' ")
     fila = cursor.fetchone()
 
